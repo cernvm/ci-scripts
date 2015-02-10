c6b21e9571b0fb04063e086119e7b4d48d80fb50
Rene Meusel <rene@renemeusel.de>
2015-02-10 10:37:28 +0000

    add first CernVM-FS build script

--- cvmfs/incremental_build.sh
+++ cvmfs/incremental_build.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
+set -e
+
 BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 . ${BUILD_SCRIPT_LOCATION}/../jenkins/common.sh
+. ${BUILD_SCRIPT_LOCATION}/common.sh
+
+# sanity checks
+[ ! -z $CVMFS_BUILD_LOCATION  ] || die "CVMFS_BUILD_LOCATION missing"
+[ ! -z $CVMFS_SOURCE_LOCATION ] || die "CVMFS_SOURCE_LOCATION missing"
+[ ! -z $CVMFS_BUILD_CLEAN     ] || die "CVMFS_BUILD_CLEAN missing"
+
+# setup a fresh build workspace on first execution or on request
+if [ ! -d "$CVMFS_BUILD_LOCATION" ] || [ x"$CVMFS_BUILD_CLEAN" = x"true" ]; then
+  rm -fR "$CVMFS_BUILD_LOCATION"
+  mkdir -p "$CVMFS_BUILD_LOCATION"
+fi
+
+# run the build
+cd "$CVMFS_BUILD_LOCATION"
+cmake -DBUILD_SERVER=yes       \
+      -DBUILD_SERVER_DEBUG=yes \
+      -DBUILD_UNITTESTS=yes    \
+      $CVMFS_SOURCE_LOCATION
+make -j $(get_number_of_cpu_cores)
