4d6b83c09bdde9a816a097b0b685c9a58dae9fe9
Rene Meusel <rene@renemeusel.de>
2015-02-10 10:37:08 +0000

    add common.sh for cvmfs CI scripts

---
+++ cvmfs/common.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# common configuration and helpers for CernVM-FS CI build scripts
+#
+
+echo
+echo "CernVM-FS Build Environment"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Build Script:            $CVMFS_BUILD_SCRIPT"
+echo "Source Location:         $CVMFS_SOURCE_LOCATION"
+echo "Build Location:          $CVMFS_BUILD_LOCATION"
+echo "Clean Build:             $CVMFS_BUILD_CLEAN"
+echo "CernVM-FS Git Revision:  $CVMFS_GIT_REVISION"
+echo
