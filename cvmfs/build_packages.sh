0f4b0360fb586edc114df4e0fe7cd38c7242c21c
Rene Meusel <rene@renemeusel.de>
2015-02-20 08:09:40 +0000

    add universal package build wrapper

---
+++ cvmfs/build_packages.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+
+BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
+. ${BUILD_SCRIPT_LOCATION}/../jenkins/common.sh
+. ${BUILD_SCRIPT_LOCATION}/common.sh
+
+# sanity checks
+[ ! -z $CVMFS_BUILD_LOCATION    ] || die "CVMFS_BUILD_LOCATION missing"
+[ ! -z $CVMFS_SOURCE_LOCATION   ] || die "CVMFS_SOURCE_LOCATION missing"
+[ ! -z $CVMFS_BUILD_SCRIPT_BASE ] || die "CVMFS_BUILD_SCRIPT_BASE missing"
+
+# setup a fresh build workspace
+if [ -d $CVMFS_BUILD_LOCATION ]; then
+  echo "removing previous build location..."
+  rm -fR $CVMFS_BUILD_LOCATION
+fi
+echo "creating a fresh build location in ${CVMFS_BUILD_LOCATION}..."
+mkdir -p "$CVMFS_BUILD_LOCATION"
+
+# run the build
+echo "looking for build script to invoke..."
+build_script_location="${CVMFS_SOURCE_LOCATION}/ci"
+build_script_name="${CVMFS_BUILD_SCRIPT_BASE}_$(get_package_type).sh"
+build_script="${build_script_location}/${build_script_name}"
+
+# check if the script exists and is executable
+[ -f $build_script ] || die "Build script '${build_script}' not found"
+[ -x $build_script ] || die "Build script '${build_script}' not executable"
+
+echo "switching to $CVMFS_BUILD_LOCATION and invoking build script..."
+cd "$CVMFS_BUILD_LOCATION"
+
+# parse the command line arguments (keep quotation marks)
+args=""
+while [ $# -gt 0 ]; do
+  if echo "$1" | grep -q "[[:space:]]"; then
+    args="$args \"$1\""
+  else
+    args="$args $1"
+  fi
+  shift 1
+done
+
+# build the invocation string and print it for debugging reasons
+command_tmpl="$build_script '${CVMFS_SOURCE_LOCATION}' '${CVMFS_BUILD_LOCATION}' $args"
+echo "++ $command_tmpl"
+
+# run the build script
+$command_tmpl
