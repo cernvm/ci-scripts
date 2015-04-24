467ee385aee18ef315ca88224335c960bf572969
Rene Meusel <rene@renemeusel.de>
2015-04-24 13:37:40 +0000

    FIX: tarball creation of aufs2-util

--- aufs/build_aufs2_utils.sh
+++ aufs/build_aufs2_utils.sh
@@ -11,6 +11,18 @@ BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 [ ! -z $AUFS_UTIL_SOURCE_LOCATION    ]                    || die "AUFS_UTIL_SOURCE_LOCATION missing"
 [ "$(echo $AUFS_UTIL_SOURCE_LOCATION | head -c1)" = "/" ] || die "AUFS_UTIL_SOURCE_LOCATION must be absolute"
 
+
+echo "define tarball and spec file specifics..."
+tarball_name="aufs2-util-2.1.tar.gz"
+tarball_prefix="$(basename $tarball_name .tar.gz)"
+tarball_path_prefix="$(echo "$AUFS_UTIL_SOURCE_LOCATION" | sed -e 's/^\/\(.*\)/\1/' | sed -e 's/\//\\\//g')"
+spec_name="aufs2-util.spec"
+
+echo "creating source tarball ($tarball_name | $tarball_path_prefix)..."
+tar --transform="s/$tarball_path_prefix/$tarball_prefix/" -cvzf \
+    $tarball_name                                               \
+    ${AUFS_UTIL_SOURCE_LOCATION}
+
 echo "cleaning out previous build location..."
 rm -fR $AUFS_UTIL_BUILD_LOCATION
 
@@ -23,14 +35,8 @@ mkdir ${AUFS_UTIL_BUILD_LOCATION}/SOURCES
 mkdir ${AUFS_UTIL_BUILD_LOCATION}/SPECS
 mkdir ${AUFS_UTIL_BUILD_LOCATION}/SRPMS
 mkdir ${AUFS_UTIL_BUILD_LOCATION}/TMP
-
-echo "creating source tarball..."
-tarball_name="aufs2-util-2.1.tar.gz"
-spec_name="aufs2-util.spec"
-
-mv      ${AUFS_UTIL_SOURCE_LOCATION}/$spec_name ${AUFS_UTIL_BUILD_LOCATION}/SPECS
-tar cfz $tarball_name                           ${AUFS_UTIL_SOURCE_LOCATION}
-mv      $tarball_name                           ${AUFS_UTIL_BUILD_LOCATION}/SOURCES
+mv ${AUFS_UTIL_SOURCE_LOCATION}/$spec_name ${AUFS_UTIL_BUILD_LOCATION}/SPECS
+mv $tarball_name                           ${AUFS_UTIL_BUILD_LOCATION}/SOURCES
 
 # Build
 echo "running the RPM build..."
