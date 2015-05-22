45907b5888016fe7e62ebb2b15a43b2a964088f0
Rene Meusel <rene@renemeusel.de>
2015-05-22 13:25:29 +0000

    allow to switch off cpplint

--- cvmfs/run_cpplint.sh
+++ cvmfs/run_cpplint.sh
@@ -9,6 +9,7 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 LINT_SCRIPT="ci/run_cpplint.sh"
 
 # sanity checks
+[ ! -z $CVMFS_RUN_CPPLINT ]                    || die "CVMFS_RUN_CPPLINT missing"
 [ ! -z $CVMFS_SOURCE_LOCATION ]                || die "CVMFS_SOURCE_LOCATION missing"
 [ ! -z $CVMFS_CPPLINT_RESULT_LOCATION ]        || die "CVMFS_CPPLINT_RESULT_LOCATION missing"
 [ -f ${CVMFS_SOURCE_LOCATION}/${LINT_SCRIPT} ] || die "$LINT_SCRIPT missing"
@@ -17,6 +18,12 @@ LINT_SCRIPT="ci/run_cpplint.sh"
 if [ -f $CVMFS_CPPLINT_RESULT_LOCATION ]; then
   echo "cleaning up old cpplint results..."
   rm -f $CVMFS_CPPLINT_RESULT_LOCATION
+fi
+
+# check if cpplint actually should be run
+if [ x"$CVMFS_RUN_CPPLINT" != x"true" ]; then
+  echo "CppLint is disabled... skipping"
+  exit 0
 fi
 
 echo "running CppLint and sending output to $CVMFS_CPPLINT_RESULT_LOCATION ..."
