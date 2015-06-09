bdad625ee58211dc15f76d182e84c35e1e680a88
Rene Meusel <rene@renemeusel.de>
2015-06-09 15:55:50 +0200

    add python unittests to run_unittests.sh

--- cvmfs/run_unittests.sh
+++ cvmfs/run_unittests.sh
@@ -7,42 +7,51 @@ BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 . ${BUILD_SCRIPT_LOCATION}/common.sh
 
 # sanity checks
-[ ! -z $CVMFS_RUN_UNITTESTS ]             || die "CVMFS_RUN_UNITTESTS missing"
-[ ! -z $CVMFS_UNITTESTS_BINARY ]          || die "CVMFS_UNITTESTS_BINARY missing"
-[ ! -z $CVMFS_UNITTESTS_RESULT_LOCATION ] || die "CVMFS_UNITTESTS_RESULT_LOCATION missing"
+[ ! -z $CVMFS_SOURCE_LOCATION ]                  || die "CVMFS_SOURCE_LOCATION missing"
+[ ! -z $CVMFS_RUN_UNITTESTS ]                    || die "CVMFS_RUN_UNITTESTS missing"
+[ ! -z $CVMFS_RUN_PYTHON_UNITTESTS ]             || die "CVMFS_RUN_PYTHON_UNITTESTS missing"
+[ ! -z $CVMFS_UNITTESTS_BINARY ]                 || die "CVMFS_UNITTESTS_BINARY missing"
+[ ! -z $CVMFS_UNITTESTS_RESULT_LOCATION ]        || die "CVMFS_UNITTESTS_RESULT_LOCATION missing"
+[ ! -z $CVMFS_UNITTESTS_PYTHON_RESULT_LOCATION ] || die "CVMFS_UNITTESTS_PYTHON_RESULT_LOCATION missing"
 
 # check if there is already a result file and clean it up
 if [ -f $CVMFS_UNITTESTS_RESULT_LOCATION ]; then
   echo "cleaning up old unittest results..."
   rm -f $CVMFS_UNITTESTS_RESULT_LOCATION
 fi
+if [ -d $CVMFS_UNITTESTS_PYTHON_RESULT_LOCATION ]; then
+  echo "cleaning up old python unittest results..."
+  rm -fR $CVMFS_UNITTESTS_PYTHON_RESULT_LOCATION
+fi
 
-# check if unittest actually should be run
-if [ x"$CVMFS_RUN_UNITTESTS" != x"true" ]; then
+# check if unittests actually should be run
+if [ x"$CVMFS_RUN_UNITTESTS" = x"true" ]; then
+  echo "running googletest unittests..."
+
+  # build the invocation string and print it for debugging reasons
+  command_tmpl="${CVMFS_SOURCE_LOCATION}/ci/run_unittests.sh"
+  [ ! -z "$CVMFS_LIBRARY_PATH" ]          && command_tmpl="$command_tmpl -l $CVMFS_LIBRARY_PATH"
+  [ x"$CVMFS_UNITTESTS_QUICK" = x"true" ] && command_tmpl="$command_tmpl -q"
+  command_tmpl="$command_tmpl ${CVMFS_UNITTESTS_BINARY} ${CVMFS_UNITTESTS_RESULT_LOCATION}"
+  echo "++ $command_tmpl"
+
+  # run the build script
+  $command_tmpl
+else
   echo "Unit tests are disabled... skipping"
-  exit 0
 fi
 
-# configure manual library path if needed
-if [ ! -z $CVMFS_LIBRARY_PATH ]; then
-  echo "using custom library path: '$CVMFS_LIBRARY_PATH'"
-  if is_linux; then
-    export LD_LIBRARY_PATH="$CVMFS_LIBRARY_PATH"
-  elif is_macos; then
-    export DYLD_LIBRARY_PATH="$CVMFS_LIBRARY_PATH"
-  else
-    die "who am i on? $(uname -a)"
-  fi
-fi
+# check if the python unittests should be run
+if [ x"$CVMFS_RUN_PYTHON_UNITTESTS" = x"true" ]; then
+  echo "running python unittests..."
 
-# check if only a quick subset of the unittests should be run
-test_filter='-'
-if [ x"$CVMFS_UNITTESTS_QUICK" = x"true" ]; then
-  echo "running only quick tests (without suffix 'Slow')"
-  test_filter='-*Slow'
+  # build the invocation string and print it for debugging reasons
+  command_tmpl="${CVMFS_SOURCE_LOCATION}/ci/run_python_unittests.sh"
+  command_tmpl="$command_tmpl ${CVMFS_UNITTESTS_PYTHON_RESULT_LOCATION}"
+  echo "++ $command_tmpl"
+
+  # run the build script
+  $command_tmpl
+else
+  echo "Python unit tests are disabled... skipping"
 fi
-# run the unit tests
-echo "running unit tests (with XML output $CVMFS_UNITTESTS_RESULT_LOCATION)..."
-$CVMFS_UNITTESTS_BINARY --gtest_shuffle                                     \
-                        --gtest_output=xml:$CVMFS_UNITTESTS_RESULT_LOCATION \
-                        --gtest_filter=$test_filter
