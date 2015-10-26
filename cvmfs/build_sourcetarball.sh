2f722364c1d96e200349055d204aac361b53d14d
Rene Meusel <rene@renemeusel.de>
2015-10-26 15:04:04 +0100

    simplify tarball creation script

--- cvmfs/build_sourcetarball.sh
+++ cvmfs/build_sourcetarball.sh
@@ -7,43 +7,27 @@ BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 . ${BUILD_SCRIPT_LOCATION}/common.sh
 
 # sanity checks
-[ ! -z $CVMFS_SOURCETARBALL_LOCATION ] || die "CVMFS_SOURCETARBALL_LOCATION missing"
-[ ! -z $CVMFS_SOURCE_LOCATION   ] || die "CVMFS_SOURCE_LOCATION missing"
-[ ! -z $CVMFS_BUILD_SCRIPT_BASE ] || die "CVMFS_BUILD_SCRIPT_BASE missing"
-[ ! -z $CVMFS_CI_PLATFORM_LABEL ] || die "CVMFS_CI_PLATFORM_LABEL missing"
-
-# setup a fresh build workspace
-if [ -d $CVMFS_SOURCE_TARBALL ]; then
-  echo "removing previous source tarball location..."
-  rm -fR $CVMFS_SOURCETARBALL_LOCATION
-fi
-echo "creating a fresh source tarball location in ${CVMFS_SOURCETARBALL_LOCATION}..."
-mkdir -p "$CVMFS_SOURCETARBALL_LOCATION"
+[ ! -z $CVMFS_SOURCE_LOCATION ] || die "CVMFS_SOURCE_LOCATION missing"
+[ ! -z $CVMFS_BUILD_LOCATION  ] || die "CVMFS_BUILD_LOCATION missing"
+[ ! -z $BUILD_NUMBER          ] || die "BUILD_NUMBER missing"
 
 # run the build
 echo "looking for build script to invoke..."
-build_script_location="${CVMFS_SOURCE_LOCATION}/ci"
-build_script_name="${CVMFS_BUILD_SCRIPT_BASE}_sourcetarball.sh"
-build_script="${build_script_location}/${build_script_name}"
+build_script="${CVMFS_SOURCE_LOCATION}/ci/build_cvmfs_sourcetarball.sh"
 
 # check if the script exists and is executable
 [ -f $build_script ] || die "Build script '${build_script}' not found"
 [ -x $build_script ] || die "Build script '${build_script}' not executable"
 
-echo "invoking source tarball maker..."
-# parse the command line arguments (keep quotation marks)
-args=""
-while [ $# -gt 0 ]; do
-  if echo "$1" | grep -q "[[:space:]]"; then
-    args="$args \"$1\""
-  else
-    args="$args $1"
-  fi
-  shift 1
-done
+# figure out if we are building a nightly
+nightly_number=
+if [ x"$CVMFS_NIGHTLY_BUILD" = x"true" ]; then
+  nightly_number=${BUILD_NUMBER}
+fi
 
+echo "invoking source tarball maker..."
 # build the invocation string and print it for debugging reasons
-command_tmpl="$build_script ${CVMFS_SOURCE_LOCATION} ${CVMFS_SOURCETARBALL_LOCATION} $args"
+command_tmpl="$build_script ${CVMFS_SOURCE_LOCATION} ${CVMFS_BUILD_LOCATION} $nightly_number"
 echo "++ $command_tmpl"
 
 # run the build script
