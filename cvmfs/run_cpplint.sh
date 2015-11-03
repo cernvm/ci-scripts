a98275d8338177bacc08bc35a29e0daca5e98414
Jakob Blomer <jblomer@cern.ch>
2015-11-03 16:38:18 +0100

    the cpplinter does not necessarily run

--- cvmfs/run_cpplint.sh
+++ cvmfs/run_cpplint.sh
@@ -48,7 +48,9 @@ $command_tmpl
 
 # chown result after cppLint on docker
 if is_docker_host; then
-  echo "chown-ing ${CVMFS_CPPLINT_RESULT_LOCATION} to $(whoami)"
-  sudo chown $(whoami) ${CVMFS_CPPLINT_RESULT_LOCATION}
+  if [ -f ${CVMFS_CPPLINT_RESULT_LOCATION} ]; then
+    echo "chown-ing ${CVMFS_CPPLINT_RESULT_LOCATION} to $(whoami)"
+    sudo chown $(whoami) ${CVMFS_CPPLINT_RESULT_LOCATION}
+  fi
 fi
 
