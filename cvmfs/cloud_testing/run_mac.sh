ac32b4a16f5e17c28f5596f842f96e681f30e772
Jose Molina <jose.molina@cern.ch>
2016-01-14 16:36:06 +0100

    FIX: use echo again instead of die

--- cvmfs/cloud_testing/run_mac.sh
+++ cvmfs/cloud_testing/run_mac.sh
@@ -16,15 +16,15 @@ client_package_url="$testee_url/$package_name"
 source_tarball_url="$testee_url/$source_name"
 
 # Step 0: check the things are there
-which vagrant                           || die "Vagrant is not installed!"
-vagrant plugin list | grep vagrant-scp  || die "Vagrant scp plugin not installed!"
+which vagrant                           || echo "Vagrant is not installed!"
+vagrant plugin list | grep vagrant-scp  || echo "Vagrant scp plugin not installed!"
 
 # Step 1: boot the VM
-vagrant up $osx_name || die "Cannot execute vagrant up $osx_name"
+vagrant up $osx_name || echo "Cannot execute vagrant up $osx_name"
 
 # Step 2: download and decompress the sources and install the client
-vagrant ssh -c "wget $source_tarball_url && tar xvf $source_name && mv cvmfs* cvmfs" $osx_name || die "Couldn't download and decompress the sources in the mac VM"
-vagrant ssh -c "wget $client_package_url && cd / && sudo /usr/sbin/installer -pkg /Users/vagrant/$package_name -target /" $osx_name || die "Couldn't download and install the CVMFS package"
+vagrant ssh -c "wget $source_tarball_url && tar xvf $source_name && mv cvmfs* cvmfs" $osx_name || echo "Couldn't download and decompress the sources in the mac VM"
+vagrant ssh -c "wget $client_package_url && cd / && sudo /usr/sbin/installer -pkg /Users/vagrant/$package_name -target /" $osx_name || echo "Couldn't download and install the CVMFS package"
 
 # Step 3: run the setup script
 vagrant ssh -c "cvmfs/test/cloud_testing/platforms/$setup_script" $osx_name
