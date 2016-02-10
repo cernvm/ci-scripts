da05e234a3267025b9f5975bc96b68809f8ac1fa
Jose Molina <jose.molina@cern.ch>
2016-02-10 11:41:27 +0100

    FIX: run_gcov_unittests.sh when making new directories

--- cvmfs/run_gcov_unittests.sh
+++ cvmfs/run_gcov_unittests.sh
@@ -16,16 +16,15 @@ BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 [ ! -z $CVMFS_SOURCE_LOCATION ] || die "CVMFS_SOURCE_LOCATION missing"
 [ ! -z $CVMFS_BUILD_CLEAN     ] || die "CVMFS_BUILD_CLEAN missing"
 
-CVMFS_XML_FILE="$CVMFS_BUILD_LOCATION/unittest.xml"
-
 # the build location will be inside the source because gcovr has problem finding the files
 CVMFS_BUILD_LOCATION="$CVMFS_SOURCE_LOCATION/build"
-mkdir -p "$CVMFS_BUILD_LOCATION"
+mkdir -p "${CVMFS_BUILD_LOCATION}"
+CVMFS_XML_FILE="$CVMFS_BUILD_LOCATION/unittest.xml"
 
 # setup a fresh build workspace on first execution or on request
 if [ ! -d "$CVMFS_BUILD_LOCATION" ] || [ x"$CVMFS_BUILD_CLEAN" = x"true" ]; then
   rm -fR "$CVMFS_BUILD_LOCATION"
-  mkdir -p "$CVMFS_BUILD_LOCATION"
+  mkdir -p "${CVMFS_BUILD_LOCATION}"
 fi
 
 # run the build
