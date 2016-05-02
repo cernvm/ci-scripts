8c188011bf947aaa8aebc0817fa03d7c80a55016
Jakob Blomer <jblomer@cern.ch>
2016-05-02 15:49:05 +0200

    build documentation in docker

--- cvmfs/build_doc.sh
+++ cvmfs/build_doc.sh
@@ -1,18 +1,15 @@
 #/bin/sh
 
-
-#
-# This script builds cvmfs_unittest_debug runs the gcov and reports the test coverage status.
-# It stores the final result in $CVMFS_
-#
-
 set -e
 
 BUILD_SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 . ${BUILD_SCRIPT_LOCATION}/../jenkins/common.sh
 . ${BUILD_SCRIPT_LOCATION}/common.sh
 
+DOXYGEN_SCRIPT="ci/run_cpplint.sh"
+
 # sanity checks
+[ ! -z $WORKSPACE ]             || die "WORKSPACE missing"
 [ ! -z $CVMFS_BUILD_LOCATION  ] || die "CVMFS_BUILD_LOCATION missing"
 [ ! -z $CVMFS_SOURCE_LOCATION ] || die "CVMFS_SOURCE_LOCATION missing"
 [ ! -z $CVMFS_BUILD_CLEAN     ] || die "CVMFS_BUILD_CLEAN missing"
@@ -24,7 +21,21 @@ if [ ! -d "$CVMFS_BUILD_LOCATION" ] || [ x"$CVMFS_BUILD_CLEAN" = x"true" ]; then
 fi
 
 # run the build
-echo "switching to ${CVMFS_BUILD_LOCATION} and invoking build script..."
-cd "${CVMFS_BUILD_LOCATION}"
-cmake -DBUILD_DOCUMENTATION=yes ${CVMFS_SOURCE_LOCATION}
-make doc
+command_tmpl=""
+desired_architecture="$(extract_arch $CVMFS_BUILD_ARCH)"
+if is_docker_host; then
+  echo "creating doxygen documentation on docker for ${desired_architecture} and sending output to $CVMFS_BUILD_LOCATION..."
+  docker_image_name="${CVMFS_BUILD_PLATFORM}_${desired_architecture}"
+  command_tmpl="${CERNVM_CI_SCRIPT_LOCATION}/docker/run_on_docker.sh \
+    ${WORKSPACE}                                                     \
+    ${docker_image_name}                                             \
+    ${CVMFS_SOURCE_LOCATION}/${DOXYGEN_SCRIPT}                       \
+    ${CVMFS_BUILD_LOCATION}"
+else
+  echo "running CppLint bare metal for ${desired_architecture}..."
+  command_tmpl="${CVMFS_SOURCE_LOCATION}/${DOXYGEN_SCRIPT} ${CVMFS_BUILD_LOCATION}"
+fi
+
+# run the build script
+echo "++ $command_tmpl"
+$command_tmpl
