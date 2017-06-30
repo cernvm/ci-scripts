727a457d304de8f3f38f8f53e8d0ec3c807076bc
Jakob Blomer <jblomer@cern.ch>
2017-06-30 11:34:10 +0200

    run cache plugin unit tests if requested

--- cvmfs/run_unittests.sh
+++ cvmfs/run_unittests.sh
@@ -43,6 +43,7 @@ if [ x"$CVMFS_RUN_UNITTESTS" = x"true" ]; then
   fi
   [ ! -z "$CVMFS_LIBRARY_PATH" ]          && command_tmpl="$command_tmpl -l $CVMFS_LIBRARY_PATH"
   [ x"$CVMFS_UNITTESTS_QUICK" = x"true" ] && command_tmpl="$command_tmpl -q"
+  [ ! -z "$CVMFS_UNITTESTS_CACHEPLUGINS" ] && command_tmpl="$command_tmpl -c $CVMFS_UNITTESTS_CACHEPLUGINS"
   command_tmpl="$command_tmpl ${CVMFS_UNITTESTS_BINARY} ${CVMFS_UNITTESTS_RESULT_LOCATION}"
   echo "++ $command_tmpl"
 
