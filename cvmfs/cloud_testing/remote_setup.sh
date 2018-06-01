6a91851561ed9f1b4de1a587364c21fa8980501a
Radu Popescu <mail@radupopescu.net>
2018-06-01 17:39:03 +0200

    Cleanup handling of repo gateway build URL

--- cvmfs/cloud_testing/remote_setup.sh
+++ cvmfs/cloud_testing/remote_setup.sh
@@ -309,9 +309,6 @@ args="-t $cvmfs_source_directory \
       -l $cvmfs_log_directory    \
       -c $client_package"
 if [ "x$(uname -s)" != "xDarwin" ]; then
-  args="$args -s $server_package -d $devel_package -g $unittest_package -k $config_package"
-  if [ "x$repository_gateway_url" != "x" ]; then
-    args="$args -w $repository_gateway_url"
-  fi
+  args="$args -s $server_package -d $devel_package -g $unittest_package -k $config_package -w $repository_gateway_url"
 fi
 sudo -H -E -u $test_username bash $platform_script_abs $args
