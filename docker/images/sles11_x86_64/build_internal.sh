c5262c79e68c29d2ae7451321398c22cd29f0f08
Jakob Blomer <jblomer@cern.ch>
2018-08-09 15:44:45 +0200

    fix sles11 building

--- docker/images/sles11_x86_64/build_internal.sh
+++ docker/images/sles11_x86_64/build_internal.sh
@@ -77,14 +77,25 @@ zypper --non-interactive     \
 
 echo "found the following public keys: "
 rpm -qa | grep gpg-pubkey
-echo "checking for expected public key..."
-expected_pubkey1="gpg-pubkey-3dbdc284-53674dd4"
-expected_pubkey2="gpg-pubkey-307e3d54-5aaa90a5"
-expected_pubkey3="gpg-pubkey-39db7c82-5847eb1f"
-[ $(rpm -qa | grep gpg-pubkey | wc -l) -eq 3 ] || { echo "more than three keys found"; exit 1; }
-rpm -qa | grep $expected_pubkey1               || { echo "public key doesn't match"; exit 1; }
-rpm -qa | grep $expected_pubkey2               || { echo "public key doesn't match"; exit 1; }
-rpm -qa | grep $expected_pubkey3               || { echo "public key doesn't match"; exit 1; }
+num_pubkeys=$(rpm -qa | grep gpg-pubkey | wc -l)
+echo "checking for expected public keys..."
+if [ $num_pubkeys -eq 3 ]; then
+  echo "checking for expected public key..."
+  expected_pubkey1="gpg-pubkey-3dbdc284-53674dd4"
+  expected_pubkey2="gpg-pubkey-307e3d54-5aaa90a5"
+  expected_pubkey3="gpg-pubkey-39db7c82-5847eb1f"
+  rpm -qa | grep $expected_pubkey1               || { echo "public key doesn't match"; exit 1; }
+  rpm -qa | grep $expected_pubkey2               || { echo "public key doesn't match"; exit 1; }
+  rpm -qa | grep $expected_pubkey3               || { echo "public key doesn't match"; exit 1; }
+elif [ $num_pubkeys -eq 2 ]; then
+  expected_pubkey1="gpg-pubkey-307e3d54-4be01a65"
+  expected_pubkey2="gpg-pubkey-3dbdc284-53674dd4"
+  rpm -qa | grep $expected_pubkey1               || { echo "public key doesn't match"; exit 1; }
+  rpm -qa | grep $expected_pubkey2               || { echo "public key doesn't match"; exit 1; }
+else
+  echo "Problem with the build container's public keys"
+  exit 1
+fi
 
 echo "rebuilding rpm database..."
 rpm_db="${DESTINATION}/var/lib/rpm"
