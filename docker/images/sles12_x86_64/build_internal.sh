2f3622f913b86e1a9eb7ea2c3d5dac1bf7cf371f
Jakob Blomer <jblomer@cern.ch>
2018-09-26 15:38:48 +0200

    fix pubkey verification for sles12 build

--- docker/images/sles12_x86_64/build_internal.sh
+++ docker/images/sles12_x86_64/build_internal.sh
@@ -63,12 +63,18 @@ echo "checking for expected public key..."
 expected_pubkey1="gpg-pubkey-3dbdc284-53674dd4"
 expected_pubkey2="gpg-pubkey-307e3d54-4be01a65"
 expected_pubkey3="gpg-pubkey-39db7c82-510a966b"
+expected_pubkey4="gpg-pubkey-307e3d54-5aaa90a5"
+expected_pubkey5="gpg-pubkey-39db7c82-5847eb1f"
 echo "*** Public Keys ***"
 rpm -qa | grep gpg-pubkey
-[ $(rpm -qa | grep gpg-pubkey | wc -l) -le 2 ] || { echo "more than two keys found"; exit 1; }
+[ $(rpm -qa | grep gpg-pubkey | wc -l) -le 3 ] || { echo "more than three keys found"; exit 1; }
 if [ $(rpm -qa | grep gpg-pubkey | wc -l) -eq 2 ]; then
   rpm -qa | grep $expected_pubkey1               || { echo "public key doesn't match ($expected_pubkey1)"; exit 1; }
   rpm -qa | grep $expected_pubkey2               || { echo "public key doesn't match ($expected_pubkey2)"; exit 1; }
+elif [ $(rpm -qa | grep gpg-pubkey | wc -l) -eq 3 ]; then
+  rpm -qa | grep $expected_pubkey1               || { echo "public key doesn't match ($expected_pubkey1)"; exit 1; }
+  rpm -qa | grep $expected_pubkey4               || { echo "public key doesn't match ($expected_pubkey4)"; exit 1; }
+  rpm -qa | grep $expected_pubkey5               || { echo "public key doesn't match ($expected_pubkey5)"; exit 1; }
 else
   rpm -qa | grep $expected_pubkey3               || { echo "public key doesn't match ($expected_pubkey3)"; exit 1; }
 fi
