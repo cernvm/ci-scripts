7e4464f835339751a7b3adb8ae3573794c120d7a
Simone Mosciatti <simone.mosciatti@cern.ch>
2019-10-03 15:12:30 +0200

    specify version of base packages

--- docker/images/cc8_x86_64/build.sh
+++ docker/images/cc8_x86_64/build.sh
@@ -10,6 +10,6 @@ BASE_ARCH="x86_64"
 REPO_BASE_URL="http://mirror.centos.org/centos/8/BaseOS/$BASE_ARCH/os"
 #REPO_BASE_URL="http://linuxsoft.cern.ch/cern/centos/8.0/os/$BASE_ARCH/"
 GPG_KEY_PATHS="http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official"
-BASE_PACKAGES="centos-release coreutils tar iputils rpm yum"
+BASE_PACKAGES="centos-release.x86_64 coreutils.x86_64 tar.x86_64 iputils.x86_64 rpm.x86_64 yum"
 
 . ${SCRIPT_LOCATION}/../rhel_common/build.sh
