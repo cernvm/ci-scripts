e2b603ba11d3038ddd84f97a52859fa6a189a075
Jan Priessnitz <jan.priessnitz@cern.ch>
2020-03-16 16:52:06 +0100

    CVMFS: add Docker image for fedora31

---
+++ docker/images/fedora31_x86_64/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
+. ${SCRIPT_LOCATION}/../common.sh
+
+SYSTEM_NAME="fedora31"
+BASE_ARCH="x86_64"
+
+yum_repo="$(get_yum_repo_mirror "https://mirrors.fedoraproject.org/mirrorlist?repo=fedora-31&arch=$BASE_ARCH")"
+REPO_BASE_URL="$yum_repo"
+REPO_MIRRORLIST="https://mirrors.fedoraproject.org/mirrorlist?repo=fedora-31&arch=$BASE_ARCH"
+GPG_KEY_PATHS="file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-31"
+BASE_PACKAGES="fedora-release coreutils tar iputils dnf"
+PACKAGE_MGR="dnf"
+
+. ${SCRIPT_LOCATION}/../rhel_common/build.sh
