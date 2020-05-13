f97e8939c81e122c3542461e8660cf6642b0a91b
Jan Priessnitz <jan.priessnitz@cern.ch>
2020-05-13 14:27:16 +0200

    Add Ubuntu 20.04 as CVMFS build platform

---
+++ docker/images/ubuntu2004_i386/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
+. ${SCRIPT_LOCATION}/../common.sh
+
+SYSTEM_NAME="ubuntu2004"
+BASE_ARCH="i386"
+REPO_BASE_URL="http://archive.ubuntu.com/ubuntu/"
+UBUNTU_RELEASE="focal"
+
+. ${SCRIPT_LOCATION}/../ubuntu_common/build.sh
