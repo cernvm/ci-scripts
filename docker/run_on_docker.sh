07bf013aee20d2c2e1a670812bc0ece7cc389285
Jakob Blomer <jblomer@cern.ch>
2020-10-09 13:41:55 +0200

    don't use sudo for docker pull

--- docker/run_on_docker.sh
+++ docker/run_on_docker.sh
@@ -29,87 +29,87 @@ which docker > /dev/null 2>&1 || die "docker is not installed"
 case ${CVMFS_DOCKER_IMAGE} in
   "ubuntu2004_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_x86_64:20.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1804_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_x86_64:18.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1804_i386")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_i386:18.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1604_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_x86_64:16.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1604_i386")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_i386:16.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1404_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_x86_64:14.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "ubuntu1404_i386")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/ubuntu_i386:14.04"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "sles11_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/sles_x86_64:11"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "sles12_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/sles_x86_64:12"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "fedora32_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/fedora_x86_64:32"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "fedora31_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/fedora_x86_64:31"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "fedora30_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/fedora_x86_64:30"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "fedora30_i386")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/fedora_i386:30"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "debian10_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/debian_x86_64:10"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "debian9_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/debian_x86_64:9"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "debian8_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/debian_x86_64:8"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "cc8_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/centos_x86_64:8"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "cc7_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/centos_x86_64:7"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "slc6_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/slc_x86_64:6"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "slc6_i386")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/slc_i386:6"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   "container_x86_64")
     image_name="gitlab-registry.cern.ch/cernvm/build-images/container_x86_64:el8"
-    sudo docker pull $image_name
+    docker pull $image_name
     ;;
   *)
     die "Unknow platform: ${CVMFS_DOCKER_IMAGE}"
