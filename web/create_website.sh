d576060b0af8915ac1474ed7046ba6913aaeb867
Jakob Blomer <jblomer@cern.ch>
2021-01-19 09:31:17 +0100

    fix website builder script

--- web/create_website.sh
+++ web/create_website.sh
@@ -5,21 +5,22 @@ pushd content
 mkdir fs appliance team
 popd
 
+USEROPT="-u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro"
 
 # we create the "main" websites using the docker image included in the repo
 pushd web-team
 docker build . -t web-team:latest
-docker run -u sftnight:sftnight -v $WORKSPACE/content/team:/root/team --rm web-team:latest bundle exec jekyll build --destination /root/team
+docker run $USEROPT -v $WORKSPACE/content/team:/root/team --rm web-team:latest bundle exec jekyll build --destination /root/team
 popd
 
 pushd web-cernvm
 docker build . -t web-cernvm:latest
-docker run -u sftnight:sftnight -v $WORKSPACE/content/appliance:/root/appliance --rm web-cernvm:latest bundle exec jekyll build --destination /root/appliance
+docker run $USEROPT -v $WORKSPACE/content/appliance:/root/appliance --rm web-cernvm:latest bundle exec jekyll build --destination /root/appliance
 popd
 
 pushd web-cvmfs
 docker build . -t web-cvmfs:latest
-docker run -u sftnight:sftnight -v $WORKSPACE/content/fs:/root/fs --rm web-cvmfs:latest bundle exec jekyll build --destination /root/fs
+docker run $USEROPT -v $WORKSPACE/content/fs:/root/fs --rm web-cvmfs:latest bundle exec jekyll build --destination /root/fs
 popd
 
 # we copy the websites to all the folders
