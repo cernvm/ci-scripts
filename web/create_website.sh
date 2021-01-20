a952ec0850fe0a56827b55c0ef3fd899a2241408
Jakob Blomer <jblomer@cern.ch>
2021-01-20 11:56:11 +0100

    fix web site builder scripts

--- web/create_website.sh
+++ web/create_website.sh
@@ -5,22 +5,22 @@ pushd content
 mkdir fs appliance team
 popd
 
-USEROPT="-u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro"
+USEROPT="-u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro"
 
 # we create the "main" websites using the docker image included in the repo
 pushd web-team
 docker build . -t web-team:latest
-docker run $USEROPT -v $WORKSPACE/content/team:/build/team --rm web-team:latest bundle exec jekyll build --destination /build/team
+docker run $USEROPT -v $WORKSPACE/content/team:/build/team --rm web-team:latest bundle exec jekyll build --disable-disk-cache --destination /build/team
 popd
 
 pushd web-cernvm
 docker build . -t web-cernvm:latest
-docker run $USEROPT -v $WORKSPACE/content/appliance:/build/appliance --rm web-cernvm:latest bundle exec jekyll build --destination /build/appliance
+docker run $USEROPT -v $WORKSPACE/content/appliance:/build/appliance --rm web-cernvm:latest bundle exec jekyll build --disable-disk-cache --destination /build/appliance
 popd
 
 pushd web-cvmfs
 docker build . -t web-cvmfs:latest
-docker run $USEROPT -v $WORKSPACE/content/fs:/build/fs --rm web-cvmfs:latest bundle exec jekyll build --destination /build/fs
+docker run $USEROPT -v $WORKSPACE/content/fs:/build/fs --rm web-cvmfs:latest bundle exec jekyll build --disable-disk-cache --destination /build/fs
 popd
 
 # we copy the websites to all the folders
