c707fe9a30d42b5978513fd2ef7fd1ae2d8ec9c0
Valentin Volkl <valentin.volkl@cern.ch>
2026-03-19 23:40:52 +0100

    try adding goproxy

--- docker/run_on_docker.sh
+++ docker/run_on_docker.sh
@@ -271,6 +271,7 @@ for var in $(env | grep -ohe "^\(CVMFS\|CERNVM\)_[^=]*"); do
   args="--env $var=$(eval "echo \$$var") $args"
 done
 args="--env GOCACHE=$WORKSPACE/.gocache $args"
+args="--env GOPROXY=http://cvm-gomod-proxy1.cern.ch:3000 $args"
 
 
 
